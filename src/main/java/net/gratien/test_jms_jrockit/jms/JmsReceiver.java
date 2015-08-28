56a65cb043bd19e803d0f2b359fca74b55d4d5ce
Gratien JAUFFRET <gratien.jauffret@gmail.com>
2015-08-28 19:30:52 +0200

    integrated oxm

--- src/main/java/net/gratien/test_jms_jrockit/jms/JmsReceiver.java
+++ src/main/java/net/gratien/test_jms_jrockit/jms/JmsReceiver.java
@@ -1,9 +1,5 @@
 package net.gratien.test_jms_jrockit.jms;
 
-import javax.jms.Message;
-import javax.jms.MessageListener;
-import javax.jms.TextMessage;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,25 +9,14 @@ import net.gratien.test_jms_jrockit.controllers.MainController;
 import net.stratml.productorservicedescription.ProductOrServiceDescription;
 
 @Component
-public class JmsReceiver implements MessageListener {
+public class JmsReceiver {
 
 	Logger logger = LoggerFactory.getLogger(JmsReceiver.class);
 
 	@Autowired
 	MainController controller;
-
-	@Override
-	public void onMessage(Message message) {
-		TextMessage msg = null;
-		try {
-			logger.info("received message : " + message);
-			msg = (TextMessage) message;
-			String xml = msg.getText();
-			controller.handleJmsMessage(xml);
-		} catch (Exception ex) {
-			if (logger.isErrorEnabled()) {
-				logger.error("Could not parse notification from queue {0} due to {1} ", new Object[] { "myQueue", ex });
-			}
-		}
+	
+	public void onProductOrServiceDescriptionMessage(ProductOrServiceDescription posd) {
+		logger.info("posd received : " + posd);
 	}
 }
--- src/test/java/net/gratien/test_jms_jrockit/jms/JmsReceiverTest.java
+++ src/test/java/net/gratien/test_jms_jrockit/jms/JmsReceiverTest.java
@@ -1,5 +1,7 @@
 package net.gratien.test_jms_jrockit.jms;
 
+import java.util.Scanner;
+
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
@@ -19,19 +21,26 @@ import net.gratien.test_jms_jrockit.config.ApplicationContextTest;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ApplicationContextTest.class)
 public class JmsReceiverTest {
-	
+
 	Logger logger = LoggerFactory.getLogger(JmsReceiverTest.class);
-	
+
 	@Autowired
 	JmsTemplate jmsTemplate;
-	
+
 	@Test
-	public void onMessageTest() throws Exception{
+	public void onMessageTest() throws Exception {
 		jmsTemplate.send(new MessageCreator() {
-			
+
+			private Scanner scanner;
+
 			@Override
 			public Message createMessage(Session session) throws JMSException {
-				return session.createTextMessage("ping!");
+				scanner = new Scanner(getClass().getResourceAsStream("/sample.xml"));
+				String xml = scanner.useDelimiter("\\A").next().replaceAll("\n", "").replaceAll("\r", "").replaceAll("\t", "");
+				logger.info("message prepared : \n" + xml);
+				Message msg = session.createTextMessage(xml);
+				msg.setJMSReplyTo(jmsTemplate.getDefaultDestination());
+				return msg;
 			}
 		});
 		logger.info("message sent");
